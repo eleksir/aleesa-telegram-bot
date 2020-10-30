eleksir/aleesa-telegram-bot

commit 033441c513eb7e8b27ebb8e6d5944920a1cba5d1

First touch to satisfy perlcritic

lib/Teapot/Bot/Brain.pm
@@ -1,7 +1,6 @@
 package Teapot::Bot::Brain;
-$Teapot::Bot::Brain::VERSION = '0.021';
-# ABSTRACT: A base class to make your very own Telegram bot
 
+# ABSTRACT: A base class to make your very own Telegram bot
 
 use Mojo::Base -base;
 
@@ -15,6 +14,8 @@ use Log::Any;
 use Data::Dumper;
 
 use Teapot::Bot::Object::Message;
+
+$Teapot::Bot::Brain::VERSION = '0.021';
 
 # base class for building telegram robots with Mojolicious
 has longpoll_time => 60;
@@ -50,6 +51,7 @@ sub add_repeating_task {
 
   # kick it off
   $repeater->();
+  return;
 }
 
 
@@ -58,6 +60,7 @@ sub add_listener {
   my $coderef = shift;
 
   push @{ $self->listeners }, $coderef;
+  return;
 }
 
 sub init {
@@ -71,6 +74,7 @@ sub think {
 
   $self->_add_getUpdates_handler;
   Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+  return;
 }
 
 
@@ -106,7 +110,6 @@ sub sendMessage {
   # check reply_markup is the right kind
   if (exists $args->{reply_markup}) {
     my $reply_markup = $args->{reply_markup};
-    die "bad reply_markup supplied"
       if ( ref($reply_markup) ne 'Teapot::Bot::Object::InlineKeyboardMarkup' &&
            ref($reply_markup) ne 'Teapot::Bot::Object::ReplyKeyboardMarkup'  &&
            ref($reply_markup) ne 'Teapot::Bot::Object::ReplyKeyboardRemove'  &&
@@ -201,6 +204,8 @@ sub _add_getUpdates_handler {
       $http_active = 0;
     });
   });
+
+  return;
 }
 
 # process a message which arrived via getUpdates
@@ -227,6 +232,8 @@ sub _process_message {
       # call the listener code, supplying ourself and the update
       $listener->($self, $update);
     }
+
+    return;
 }
 
 

lib/Teapot/Bot.pm
@@ -1,10 +1,9 @@
 package Teapot::Bot;
-$Teapot::Bot::VERSION = '0.021';
+
 use strict;
 use warnings;
-
+$Teapot::Bot::VERSION = '0.021';
 # ABSTRACT: A base class to make your very own Telegram bot
-
 
 1;
 

lib/Teapot/Bot/Object/Animation.pm
@@ -1,11 +1,11 @@
 package Teapot::Bot::Object::Animation;
-$Teapot::Bot::Object::Animation::VERSION = '0.021';
 # ABSTRACT: The base class for Telegram message 'Animation' type.
-
 
 use Mojo::Base 'Teapot::Bot::Object::Base';
 use Teapot::Bot::Object::PhotoSize;
 use Carp qw/croak/;
+
+$Teapot::Bot::Object::Animation::VERSION = '0.021';
 
 has 'file_id';
 has 'width';

lib/Teapot/Bot/Object/Audio.pm
@@ -1,9 +1,9 @@
 package Teapot::Bot::Object::Audio;
-$Teapot::Bot::Object::Audio::VERSION = '0.021';
 # ABSTRACT: The base class for Telegram 'Audio' type objects
 
-
 use Mojo::Base 'Teapot::Bot::Object::Base';
+
+$Teapot::Bot::Object::Audio::VERSION = '0.021';
 
 has 'file_id';
 has 'duration';

lib/Teapot/Bot/Object/Base.pm
@@ -1,9 +1,9 @@
 package Teapot::Bot::Object::Base;
-$Teapot::Bot::Object::Base::VERSION = '0.021';
 # ABSTRACT: The base class for all Teapot::Bot::Object objects.
 
-
 use Mojo::Base -base;
+
+$Teapot::Bot::Object::Base::VERSION = '0.021';
 
 has '_brain'; # a reference to our brain
 

lib/Teapot/Bot/Object/Chat.pm
@@ -1,11 +1,11 @@
 package Teapot::Bot::Object::Chat;
-$Teapot::Bot::Object::Chat::VERSION = '0.021';
 # ABSTRACT: The base class for Telegram 'Chat' type objects
-
 
 use Mojo::Base 'Teapot::Bot::Object::Base';
 use Teapot::Bot::Object::ChatPhoto;
 use Teapot::Bot::Object::Message;
+
+$Teapot::Bot::Object::Chat::VERSION = '0.021';
 
 has 'id';
 has 'type';
@@ -34,12 +34,12 @@ sub fields {
 
 
 sub is_user {
-  shift->id > 0;
+  return shift->id > 0;
 }
 
 
 sub is_group {
-  shift->id < 0;
+  return shift->id < 0;
 }
 
 1;

lib/Teapot/Bot/Object/ChatPhoto.pm
@@ -1,9 +1,9 @@
 package Teapot::Bot::Object::ChatPhoto;
-$Teapot::Bot::Object::ChatPhoto::VERSION = '0.021';
 # ABSTRACT: The base class for Telegram 'ChatPhoto' type objects
 
-
 use Mojo::Base 'Teapot::Bot::Object::Base';
+
+$Teapot::Bot::Object::ChatPhoto::VERSION = '0.021';
 
 has 'small_file_id';
 has 'big_file_id';

lib/Teapot/Bot/Object/Contact.pm
@@ -1,9 +1,9 @@
 package Teapot::Bot::Object::Contact;
-$Teapot::Bot::Object::Contact::VERSION = '0.021';
 # ABSTRACT: The base class for Telegram 'Contact' objects.
 
-
 use Mojo::Base 'Teapot::Bot::Object::Base';
+
+$Teapot::Bot::Object::Contact::VERSION = '0.021';
 
 has 'phone_number';
 has 'first_name';

commit 29c1fa8d07d9f489ae089ec3773b0ccdcf1c95b5

Breaking API changes (handle Mojo loop errors), fix sendMessage method broken in 033441c
* Use general method to handle Mojo loop unsuccessful operations
* Do not die if tg api returns error via Mojo loop, lift up error status to upper layer
* Die with confess only in case of Mojo loop does not returns either is_success or is_error set
* Handle incomming updates related to poll event
* Update inline POD in Brain.pm
* Fix overlooked die removal in sendMessage, now it is croak

lib/Teapot/Bot/Brain.pm
@@ -94,7 +94,7 @@ sub getMe {
   }
   else {
     return {"error" => 1};
-  } 
+  }
 }
 
 
@@ -149,10 +149,10 @@ sub sendMessage {
   my $args = shift || {};
 
   my $send_args = {};
-  croak "no chat_id supplied" unless $args->{chat_id};
+  croak "no chat_id supplied in sendMessage" unless $args->{chat_id};
   $send_args->{chat_id} = $args->{chat_id};
 
-  croak "no text supplied"    unless $args->{text};
+  croak "no text supplied in sendMessage"    unless $args->{text};
   $send_args->{text}    = $args->{text};
 
   # these are optional, send if they are supplied
@@ -164,14 +164,14 @@ sub sendMessage {
   # check reply_markup is the right kind
   if (exists $args->{reply_markup}) {
     my $reply_markup = $args->{reply_markup};
-      croak "Incorrect reply_markup" if ( ref($reply_markup) ne 'Teapot::Bot::Object::InlineKeyboardMarkup' &&
+      croak "Incorrect reply_markup in sendMessage" if ( ref($reply_markup) ne 'Teapot::Bot::Object::InlineKeyboardMarkup' &&
            ref($reply_markup) ne 'Teapot::Bot::Object::ReplyKeyboardMarkup'  &&
            ref($reply_markup) ne 'Teapot::Bot::Object::ReplyKeyboardRemove'  &&
            ref($reply_markup) ne 'Teapot::Bot::Object::ForceReply' );
     $send_args->{reply_markup} = $reply_markup;
   }
 
-  my $token = $self->token || croak "no token?";
+  my $token = $self->token || croak "no token in sendMessage?";
   my $url = "https://api.telegram.org/bot${token}/sendMessage";
   my $api_response = $self->_post_request($url, $send_args);
 
@@ -188,19 +188,19 @@ sub forwardMessage {
   my $self = shift;
   my $args = shift || {};
   my $send_args = {};
-  croak "no chat_id supplied" unless $args->{chat_id};
+  croak "no chat_id supplied in forwardMessage" unless $args->{chat_id};
   $send_args->{chat_id} = $args->{chat_id};
 
-  croak "no from_chat_id supplied"    unless $args->{from_chat_id};
+  croak "no from_chat_id supplied in forwardMessage"    unless $args->{from_chat_id};
   $send_args->{from_chat_id}    = $args->{from_chat_id};
 
-  croak "no message_id supplied"    unless $args->{message_id};
+  croak "no message_id supplied in forwardMessage"    unless $args->{message_id};
   $send_args->{message_id}    = $args->{message_id};
 
   # these are optional, send if they are supplied
   $send_args->{disable_notification} = $args->{disable_notification} if exists $args->{disable_notification};
 
-  my $token = $self->token || croak "no token?";
+  my $token = $self->token || croak "no token in forwardMessage?";
   my $url = "https://api.telegram.org/bot${token}/forwardMessage";
   my $api_response = $self->_post_request($url, $send_args);
 
@@ -225,7 +225,7 @@ sub sendPhoto {
   # to fetch, or a file_id string) or a file on disk to upload - we need
   # to handle that last case here as it changes the way we create the HTTP
   # request
-  croak "no photo supplied" unless $args->{photo};
+  croak "no photo supplied in sendPhoto" unless $args->{photo};
   if (-e $args->{photo}) {
     $send_args->{photo} = { photo => { file => $args->{photo} } };
   }
@@ -233,7 +233,7 @@ sub sendPhoto {
     $send_args->{photo} = $args->{photo};
   }
 
-  my $token = $self->token || croak "no token?";
+  my $token = $self->token || croak "no token in sendPhoto?";
   my $url = "https://api.telegram.org/bot${token}/sendPhoto";
   my $api_response = $self->_post_request($url, $send_args);
 
@@ -251,7 +251,7 @@ sub sendChatAction {
   my $args = shift || {};
   my $send_args = {};
 
-  croak "no chat_id supplied" unless $args->{chat_id};
+  croak "no chat_id supplied in sendChatAction" unless $args->{chat_id};
   $send_args->{chat_id} = $args->{chat_id};
 
   if ($args->{action} eq 'upload_photo') {
@@ -285,7 +285,7 @@ sub sendChatAction {
     $send_args->{action} = 'typing';
   }
 
-  my $token = $self->token || croak "no token?";
+  my $token = $self->token || croak "no token in sendChatAction?";
   my $url = "https://api.telegram.org/bot${token}/sendChatAction";
   my $api_response = $self->_post_request ($url, $send_args);
 